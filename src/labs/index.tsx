64b4c8f9b4b101f0b2446562bdc9ef5d82cc4b7a
andrewturcotte166 <turcotte.an@northeastern.edu>
2024-03-17 11:11:13 -0400

    lab5 progress

--- src/labs/index.tsx
+++ src/labs/index.tsx
@@ -2,6 +2,7 @@ import Nav from "../Nav";
 import Assignment3 from "./a3";
 import { Routes, Route, Navigate }
     from "react-router";
+import Assignment5 from "./a5";
 function Labs() {
     return (
         <div>
@@ -12,6 +13,8 @@ function Labs() {
                         to="a3" />} />
                 <Route path="a3"
                     element={<Assignment3 />} />
+                <Route path="a5"
+                    element={<Assignment5 />} />
             </Routes>
         </div>
     );
